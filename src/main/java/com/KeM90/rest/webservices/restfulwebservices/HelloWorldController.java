468823b1e1e02ba29946088f98b069f545ad3f20
KeM90 <k.meier.09@gmx.de>
2024-04-08 04:08:40 +0200

    Connect to another host

--- src/main/java/com/KeM90/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/KeM90/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,11 +1,13 @@
 package com.KeM90.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
 //Controller
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class HelloWorldController {
 
@@ -21,7 +23,8 @@ public class HelloWorldController {
     //hello-world-bean
     @GetMapping(path="/hello-world-bean") 
     public HelloWorldBean helloWorldBean(){
-        return new HelloWorldBean("Hello World");
+        throw new RuntimeException("Some Error has Happened! Contact Support at ***-***");
+        //return new HelloWorldBean("Hello Worl - Changed");
     }
 
     //hello-world/path-variable/name
